5c795dc8157f0518ba56308f563f0f471b32c363
yimy <yimy@DEVELOPER>
2021-09-26 09:11:02 -0400

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+G
+parenthesis_checker.iml,7\0\70cc42ac6048cc85c30b6d255acd680451ab6d51
---
+++ src/ParenthesisChecker.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class ParenthesisChecker {
+
+    public static void main(String[] args) {
+        System.out.println(validate(" 1 + (2+4)"));  // Correct
+        System.out.println(validate(")(3+2)"));      // Incorrect
+        System.out.println(validate("((3+2)"));      // Incorrect
+        System.out.println(validate("(3+2()"));      // Incorrect
+        System.out.println(validate("(3+2())"));     // Correct
+        System.out.println(validate("(3+2())()()()()jjfjfjfjfj("));     // InCorrect
+    }
+
+    public static String validate(String text) {
+
+        // Replace the synchronized class "Stack" by an unsynchronized one such as "Deque". for a better performance
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : text.trim().toCharArray()) {
+
+            if (c == '(') {
+                stack.push(c);
+            }
+            else if( c == ')' && stack.isEmpty()) {
+                return "Incorrect";
+            }
+            else if(c == ')')
+            {
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty())
+            return "Correct";
+        else
+            return "Incorrect";
+    }
+}
